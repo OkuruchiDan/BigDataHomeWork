beb8e55845fa49cd3669637495128fc86778d6e1
Bogdan Skripnik <bogdan3508@gmail.com>
2020-08-12 19:45:01 +0300

    Reading 4 fields and creating InfoAboutOneAVGTIP objects.

---
+++ src/main/java/InfoAboutOneAVGTIP.java
@@ -0,0 +1,75 @@
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+@Data
+@NoArgsConstructor
+public class InfoAboutOneAVGTIP {
+    private int time_of_day;
+    private String trip_distance;
+    private String payment_type;
+    private String tip_amount;
+
+    @Override
+    public String toString() {
+        return "InfoAboutOneAVGTIP{" +
+                "time_of_day='" + time_of_day + '\'' +
+                ", trip_distance='" + trip_distance + '\'' +
+                ", payment_type='" + payment_type + '\'' +
+                ", tip_amount='" + tip_amount + '\'' +
+                '}';
+    }
+
+    public int getTime_of_day() {
+        return time_of_day;
+    }
+
+    public void setTime_of_day(int time_of_day) {
+        this.time_of_day = time_of_day;
+    }
+
+    public String getTrip_distance() {
+        return trip_distance;
+    }
+
+    public void setTrip_distance(String trip_distance) {
+        this.trip_distance = trip_distance;
+    }
+
+    public String getPayment_type() {
+        return payment_type;
+    }
+
+    public void setPayment_type(String payment_type) {
+        this.payment_type = payment_type;
+    }
+
+    public String getTip_amount() {
+        return tip_amount;
+    }
+
+    public void setTip_amount(String tip_amount) {
+        this.tip_amount = tip_amount;
+    }
+
+    public InfoAboutOneAVGTIP(String tpep_pickup_datetime, String trip_distance, String payment_type, String tip_amount) {
+        SimpleDateFormat formatPattern = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        try {
+            Date date = formatPattern.parse(tpep_pickup_datetime);
+            Calendar calendar = GregorianCalendar.getInstance();
+            calendar.setTime(date);
+            this.time_of_day = calendar.get(Calendar.HOUR_OF_DAY);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.trip_distance = trip_distance;
+        this.payment_type = payment_type;
+        this.tip_amount = tip_amount;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,19 @@
-import au.com.bytecode.opencsv.CSVReader;
 
-import java.io.File;
+
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderHeaderAware;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.apache.commons.csv.CSVParser;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,15 +21,26 @@ public class Main {
         String pathToFile = "/home/bogdan/Programming/HomeWorkBigData/yellow_tripdata_2018-01.csv";
         try (FileReader fileReader = new FileReader(pathToFile)) { // this initialization will close Reader stream automatically
             // create CSVReader from OpenCSV lib
-            CSVReader csvReader = new CSVReader(fileReader, ',', '"');
+
+            CSVReader csvReader = new CSVReader(fileReader);
+            String[] header = csvReader.readNext();
+
+            CSVParserBuilder csvParserBuilder = new CSVParserBuilder();
+
             // create ArrayList with 1500 capacity fore store all data
             List<String[]> allData = new ArrayList<>(1500);
             // Every next string will be printed and add to ArrayList
-            String[] nextLine; // this array of string will be always redefined
-            while((nextLine = csvReader.readNext()) != null){
-                System.out.println(Arrays.toString(nextLine));
-                allData.add(nextLine); // add to ArrayList
+            String[] nextLine = csvReader.readNext(); // this array of string will be always redefined
+            System.out.println(Arrays.toString(nextLine));
+
+            int i = 0;
+            List<InfoAboutOneAVGTIP> infoAboutOneAVGTIPSList = new ArrayList<>(1500);
+            while((nextLine = csvReader.readNext()) != null && i <= 1500){
+                i++;
+                InfoAboutOneAVGTIP infoAboutOneAVGTIP = new InfoAboutOneAVGTIP(nextLine[1], nextLine[4], nextLine[9], nextLine[13]);
+                infoAboutOneAVGTIPSList.add(infoAboutOneAVGTIP);
             }
+            infoAboutOneAVGTIPSList.forEach(System.out::println);
         } catch (IOException e) { // Handling IO from FileReader
             e.printStackTrace();
         }
