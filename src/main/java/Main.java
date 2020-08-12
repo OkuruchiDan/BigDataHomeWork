3265a2ad0b1b161f0da3aa0191e7f2edfe195f56
Bogdan Skripnik <bogdan3508@gmail.com>
2020-08-12 13:32:37 +0300

    Check for file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helelle");
+        //Path to .csv file
+        String pathToFile = "/home/bogdan/Programming/HomeWorkBigData/yellow_tripdata_2018-01.csv";
+        File file = new File(pathToFile);
+        System.out.println(file.canRead()); // checking readable of the file.
+
     }
 }
